2625449b7c81c484b8e86baa8e76fdc28c9904fd
GeekyMovies <121683432+GeekyMovies@users.noreply.github.com>
2023-01-07 13:24:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ API_ID = os.environ.get("API_ID", "23311160")
 
 API_HASH = os.environ.get("API_HASH", "2a1366013eca4256bce853346dbcda49")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5840549862:AAHjFY5SGuyxaEw97EXhlOqAp1Z5ROC6WwU") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5914875548:AAF9MzN2Mk8QQhkalABpz_45MjQjBnQUOqg") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "@geeky_movies") 
 
 DB_NAME = os.environ.get("DB_NAME","geekymovies")     
 
